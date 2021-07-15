6afa2d490a8ba16ac6461ef0d72b67df831a615a
Thomas Moore <tom@Thomass-MacBook-Air.local>
2021-07-15 23:54:34 +0100

    remove lib

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/server';
-import {hydrate} from 'react-dom';
-import App from'./App';
+import React from "react";
+import ReactDOM from "react-dom/server";
+import { hydrate } from "react-dom";
+import App from "./App";
 
-const Html = props => {
+const Html = (props) => {
   const html = { __html: props.children };
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <title>App 2</title>
-        <link rel='stylesheet' href='/app.css' />
+        <title>App</title>
+        <link rel="stylesheet" href="/app.css" />
       </head>
       <body>
-        <div id='app' dangerouslySetInnerHTML={html} />
-        <script src='/app.js'></script>
+        <div id="app" dangerouslySetInnerHTML={html} />
+        <script src="/app.js"></script>
       </body>
     </html>
   );
 };
 
-const doc = '<!doctype html>' + ReactDOM.renderToStaticMarkup(
-  <Html>
-    {ReactDOM.renderToString(<App />)}
-  </Html>
-);
+const doc =
+  "<!doctype html>" +
+  ReactDOM.renderToStaticMarkup(
+    <Html>{ReactDOM.renderToString(<App />)}</Html>
+  );
 
-
-if (typeof document === 'undefined') {
-  const fs = require(/* webpackIgnore: true */ 'fs');
-  fs.writeFileSync('./dist/index.html', doc);
+if (typeof document === "undefined") {
+  const fs = require(/* webpackIgnore: true */ "fs");
+  fs.writeFileSync("./dist/index.html", doc);
 } else {
-  hydrate(<App/>, document.getElementById('app'));
+  hydrate(<App />, document.getElementById("app"));
 }
 
 console.log(`File written: ${process.cwd()}/index.html`);
